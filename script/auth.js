559ba2c91124e795149b533b0a7ae42e34034d0c
tash-9 <3222.tasfiaislam@gmail.com>
2026-03-10 00:54:03 +0600

    Refactoring Final

--- script/auth.js
+++ script/auth.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   if (localStorage.getItem("isLoggedIn") === "true") {
 
-    document.getElementById("login-section").style.display = "none";
-    document.getElementById("dashboard-section").style.display = "block";
+    document.getElementById("login-section").classList.add("hidden");
+    document.getElementById("dashboard-section").classList.remove("hidden");
 
     if (typeof loadIssues === "function") {
       loadIssues();
     }
 
-  } else {
-
-    document.getElementById("login-section").style.display = "block";
-    document.getElementById("dashboard-section").style.display = "none";
-
   }
+
 });
